c3bcf996c9536e2c862be258806ec11a428fe356
Bartekkz <bartosz.szmelczynski@gmail.com>
2020-03-04 00:40:05 +0100

    add backprop

--- maxpool.py
+++ maxpool.py
@@ -14,14 +14,14 @@ class MaxPooling2d:
     
     def iterate_regions(self, input):
         """
-        Generates non-overlapping (pool_size, pool_size) inputs regions to pool over
+        Generates non-overlapping (2, 2) inputs regions to pool over
         - input: 2d numpy array 
         """
         
         h, w, _ = input.shape
         
-        new_h = (h - self.pool_size) // 2
-        new_w = (w - self.pool_size) // 2
+        new_h = h // 2
+        new_w = h // 2
         
         for i in range(new_h):
             for j in range(new_w):
@@ -38,7 +38,7 @@ class MaxPooling2d:
         self.last_input = input
         
         h, w, num_filters = input.shape
-        output = np.zeros(((h - self.pool_size) // 2, (w - self.pool_size) // 2, num_filters))
+        output = np.zeros((h // 2, h // 2, num_filters))
         
         for input_region, i, j in self.iterate_regions(input):
             output[i, j] = np.amax(input_region, axis=(0, 1))
@@ -51,7 +51,18 @@ class MaxPooling2d:
         Returns the loss gradient for this layer's inputs.
         - d_L_d_out is the loss gradient for this layer's outputs.
         """
+        d_L_d_input = np.zeros(self.last_input.shape)
         
+        for input_region, i, j in self.iterate_regions(self.last_input):
+            h, w, f = input.shape
+            amax = np.amax(input_region, axis=(0, 1))
+            
+            for i2 in range(h):
+              for j2 in range(w):
+                  for f2 in range(f):
+                    # If this pixel was the max value, copy the gradient to it.
+                    if input_region[i2, j2, f2] == amax[f2]:
+                        d_L_d_input[i * 2 + i2, j * 2 + j2, f2] = d_L_d_out[i, j, f2]
         
-        
+        return d_L_d_input
 
